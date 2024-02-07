5441d6fcd770845a7d59db35ee9e080da2a48746
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-02-06 23:31:41 -0500

    -renaming few files -Redid adding the second class(did it correctly this time) -Made the background light blue

--- MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
+++ MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
@@ -1,10 +1,14 @@
-//Mathew Anderson, n01436706
 package mathew.anderson.n01436706;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MathewActivity2 extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mathew2);
+    }
 }
