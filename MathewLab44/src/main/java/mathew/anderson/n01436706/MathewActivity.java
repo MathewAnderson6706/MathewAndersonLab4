e378969e7898d665bfcaaa17944dc11b24a515ae
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-02-07 15:21:49 -0500

    -Renamed textview -Added EditText -Added Button -Added Snackbar when Button clicked -Added ability to go to next screen when START is clicked -Value should be transfered over

--- MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity.java
+++ MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity.java
@@ -3,13 +3,58 @@ package mathew.anderson.n01436706;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MathewActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mathew);
+
+
+        Button btnOpen = (Button) findViewById(R.id.btnOpen);
+        //register the button with an event listener
+        btnOpen.setOnClickListener(new View.OnClickListener()
+        { //anonymous class
+            //implement your event handler method
+            public void onClick(View v) {
+                EditText editText = findViewById(R.id.txtName);
+                String text = editText.getText().toString();
+                Long num;
+
+                if (!text.isEmpty()) {
+                    num = Long.valueOf(text);
+                } else {
+                    num = -1000L;
+                }
+
+                Intent intent = new Intent(MathewActivity.this, MathewActivity2.class);
+                intent.putExtra("key", num);
+
+                CharSequence clicked = getString(R.string.snk);
+                CharSequence start = getString(R.string.start);
+                Snackbar.make(findViewById(android.R.id.content), clicked, Snackbar.LENGTH_INDEFINITE)
+                        .setAction(start, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                startActivity(intent);
+                            }
+                        })
+                        .show();
+            }
+
+
+        });
     }
+
 }
--- MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
+++ MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
@@ -2,6 +2,7 @@ package mathew.anderson.n01436706;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MathewActivity2 extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MathewActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mathew2);
+
+        //Intent intent = getIntent();
     }
 }
