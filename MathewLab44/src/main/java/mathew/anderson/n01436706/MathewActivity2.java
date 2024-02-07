698a183317d98194bc694276b549254af50eefab
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-02-07 15:59:09 -0500

    -Second Screen can now display the numbers

--- MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
+++ MathewLab44/src/main/java/mathew/anderson/n01436706/MathewActivity2.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class MathewActivity2 extends AppCompatActivity {
+
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mathew2);
 
-        //Intent intent = getIntent();
+        textview = findViewById(R.id.txtView2);
+        Intent intent = getIntent();
+        long num = intent.getLongExtra("key", 0);
+        String text = "Number: " + num;
+        textview.setText(text);
     }
 }
